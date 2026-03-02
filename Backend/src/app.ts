4fa1164042d2cc4a54a9925d8852b065c11da9a8
JawadHamdan03 <eng.jawad121@hotmail.com>
2026-03-03 01:08:59 +0200

    Backend Service Dockerfile

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -3,6 +3,8 @@ import dotenv from "dotenv";
 import authRoutes from "./routes/authRoutes";
 import userRoutes from "./routes/userRoutes"
 dotenv.config();
+const PORT = Number(process.env.PORT) || 5000
+
 
 const app = express();
 
@@ -12,4 +14,4 @@ app.use(express.json());
 app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
 
-app.listen(5000, () => console.log("server is running on port 5000"));
+app.listen(PORT, () => console.log(`server is running on port ${PORT}`));
