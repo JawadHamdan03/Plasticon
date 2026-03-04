52c2df69fa152203adabca2c4f0a32cc627d227d
JawadHamdan03 <eng.jawad121@hotmail.com>
2026-03-04 22:39:35 +0200

    fix auth middleware

--- Backend/src/routes/userRoutes.ts
+++ Backend/src/routes/userRoutes.ts
@@ -1,11 +1,9 @@
 import { Router } from 'express'
 import { getUsers, getUserById, deleteUser } from '../controllers/userController'
-import { authMiddleware, authorizeRoles } from "../middleware/authMiddleware";
+import { authorizeRoles } from "../middleware/authMiddleware";
 import { UserRole } from "../config/generated/prisma/client";
 
 const router = Router()
-
-router.use(authMiddleware)
 
 router.get("/all", authorizeRoles([UserRole.ADMIN]), getUsers)
 router.get("/:id", authorizeRoles([UserRole.ADMIN]), getUserById)
