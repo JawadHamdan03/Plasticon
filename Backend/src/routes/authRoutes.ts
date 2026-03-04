9398ce129fc5abc9de74645dc935515a7dac1da5
JawadHamdan03 <eng.jawad121@hotmail.com>
2026-03-04 02:22:41 +0200

    change register authorization to admin only

--- Backend/src/routes/authRoutes.ts
+++ Backend/src/routes/authRoutes.ts
@@ -1,9 +1,11 @@
 import { Router, type Request, type Response } from "express";
 import { registerHandler, loginHandler, logoutHandler } from "../controllers/authController";
+import { authMiddleware, authorizeRoles } from "../middleware/authMiddleware";
+import { UserRole } from "../config/generated/prisma/enums";
 
 const router = Router();
 
-router.post("/register", registerHandler);
+router.post("/register", authMiddleware, authorizeRoles([UserRole.ADMIN]), registerHandler);
 
 router.post("/login", loginHandler);
 
